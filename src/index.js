7e112a94f447caf03f72c88c564fbd5aaddbe7ca
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-07-31 14:11:55 -0700

    Add file structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import './css/index.css';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
