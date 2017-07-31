d97412509149e8165572868229496dbbeb318130
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-07-31 16:02:20 -0700

    Add temp eslint disable for app and index since current configuration does not reflect near future

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+/* eslint-disable react/jsx-filename-extension */
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
