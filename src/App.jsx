42628f6b2568b09d430d4148acd56effa72de234
Tyler <misterlango@gmail.com>
2017-07-31 16:07:36 -0700

    Add file structure (#1)
    * Add dependencies
    
    * Add file structure
    
    * Change package json to only use fixed versions, remove babel presets, and add a few additional dependencies
    
    * Move css files right next to their components
    
    * Rename components to use jsx file extension
    
    * Add temp eslint disable for app and index since current configuration does not reflect near future
    
    * Remove rogue caret

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+/* eslint-disable react/prefer-stateless-function */
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to the Transit SPA</h2>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+/* eslint-disable react/jsx-filename-extension */
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
