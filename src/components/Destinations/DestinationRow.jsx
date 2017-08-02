96d738c2839591ef7ecedac3326da9ad414ea8d9
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-08-02 11:52:39 -0700

    Add destination row component

---
+++ src/components/Destinations/DestinationRow.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Container, Segment } from 'semantic-ui-react';
+
+import JourneyTable from '../Journeys/JourneyTable';
+import DestinationHeader from './Destination/DestinationHeader';
+import './destination.css';
+
+const DestinationRow = ({ id, name }) =>
+  (<Container>
+    <Segment compact>
+      <DestinationHeader name={name} />
+    </Segment>
+    <Segment compact textAlign="center">
+      <JourneyTable id={id} />
+    </Segment>
+  </Container>);
+
+DestinationRow.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+DestinationRow.defaultProps = {
+  id: 1,
+  name: '',
+};
+
+export default DestinationRow;
