960fd75427d35baf06f4572f860d8afc6eb80e97
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-13 03:05:35 -0700

    Remove obsolete file and tests for file

--- src/components/AddNewDestination/AddNewDestinationRow.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AddNewDestinationModal from './AddNewDestinationModal';
-
-const AddNewDestinationRow = () => <AddNewDestinationModal />;
-
-export default AddNewDestinationRow;
