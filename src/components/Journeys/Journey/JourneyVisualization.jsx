e5dced88bd056db0569903f4f1a9ef7b46c9c2e2
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-13 03:37:17 -0700

    Trim down journey visualization concerns and move composing functions to another file

--- src/components/Journeys/Journey/JourneyVisualization.jsx
+++ src/components/Journeys/Journey/JourneyVisualization.jsx
@@ -1,77 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Icon, List, Step } from 'semantic-ui-react';
+import { Step } from 'semantic-ui-react';
 
-import './journey-visualization.css';
+import TransitNode from './TransitNode';
 
-function formatAgency(step) {
-  let output = '';
-  if (step.mode.includes('WALKING')) {
-    output = 'Walk';
-  }
-  if (step.agency) {
-    output = step.agency.replace(/Bay Area Rapid Transit/, 'BART');
-  }
-  return output;
-}
+const JourneyVisualization = ({ steps }) => (
+  <Step.Group className="journeyVizColumn">
+    {steps.map((step) => {
+      const uniqueKey = step.shortName + step.instruction;
 
-function getInstructions(step) {
-  let instr = '';
-  if (!step) {
-    return instr;
-  }
-  if (step.shortName) {
-    if (step.instruction.startsWith('Bus ')) {
-      instr = step.instruction.replace(/^Bus /, `Bus ${step.shortName} `);
-    } else {
-      instr = `${step.shortName} ${step.instruction}`;
-    }
-  } else {
-    instr = step.instruction;
-  }
-  instr = instr.replace(/^Walk /, '');
-  instr = instr.replace(/San Francisco International Airport/, 'SF Intl. Airport');
-  return instr;
-}
-
-const JourneyVisualization = ({ active, steps }) =>
-  (<List.Item>
-    <Step.Group>
-      {steps.map((step, index) => {
-        let name;
-
-        if (step.mode === 'WALKING' && index !== 0) return null;
-
-        if (step.mode.includes('WALKING')) {
-          name = 'blind';
-        }
-
-        if (step.mode === 'TRANSIT') {
-          name = step.instruction.includes('rail') ? 'subway' : 'bus';
-        }
-
-        const uniqueKey = step.shortName + step.instruction;
-        const agency = formatAgency(step);
-        const instructions = getInstructions(step);
-
-        return (
-          <Step disabled={active} key={uniqueKey}>
-            <Icon name={name} size="big" />
-            <Step.Content title={agency} description={instructions} />
-          </Step>
-        );
-      })}
-    </Step.Group>
-  </List.Item>);
+      return (
+        <Step key={uniqueKey}>
+          <TransitNode step={step} />
+        </Step>
+      );
+    })}
+  </Step.Group>
+);
 
 JourneyVisualization.propTypes = {
-  active: PropTypes.bool.isRequired,
   steps: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 JourneyVisualization.defaultProps = {
-  active: null,
   steps: [{}],
 };
 
