d2f0b94fd72056ca499a180f1603ca073aadafa7
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-13 17:52:17 -0700

    Move api directions data processing out of action creator

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -6,15 +6,61 @@ export default class TRANSIT_API {
     return fetch(
       `${apiUrl}/directions?origin=${origin}&destination=${destination}&alternatives=true`,
     )
-    .then(response => response.json())
-    .catch(e => e);
+      .then(response => response.json())
+      .then(json =>
+        json.map((rawJourneyObj) => {
+          const journeyObj = {
+            destination: rawJourneyObj.legs[0].end_address,
+            arrivalTimeText: rawJourneyObj.legs[0].arrival_time.text,
+            departureTimeUTC: rawJourneyObj.legs[0].departure_time.value,
+
+            transitSteps: rawJourneyObj.legs[0].steps.map((step) => {
+              const mode = step.travel_mode;
+              const duration = step.duration.text;
+              const instruction = step.html_instructions;
+
+              const agency = step.transit_details ? step.transit_details.line.agencies[0].name : '';
+              const line = step.transit_details ? step.transit_details.line.short_name : 'N/A';
+              const vehicle = step.transit_details ? step.transit_details.line.vehicle.name : 'N/A';
+              const shortName = step.transit_details ? step.transit_details.line.short_name : ''; // mostly for buses
+              const longName = step.transit_details ? step.transit_details.line.name : ''; // mostly for BART
+              const headsign = step.transit_details ? step.transit_details.headsign : '';
+              const icon = step.transit_details ? step.transit_details.line.icon : 'N/A'; // mostly for buses
+              const localIcon = step.transit_details
+                ? step.transit_details.line.vehicle.local_icon
+                : 'N/A'; // mostly for BART
+
+              const stepObj = {
+                mode,
+                duration,
+                instruction,
+                agency,
+                line,
+                vehicle,
+                shortName,
+                longName,
+                headsign,
+                icon,
+                localIcon,
+              };
+              return stepObj;
+            }),
+          };
+
+          return journeyObj;
+        }),
+      )
+      .then(journeys => journeys)
+      .catch((e) => {
+        console.dir('ERROR IN FETCH JOURNEYS', e);
+      });
   }
 
   static getCurrentLocation() {
     const getPosition = options =>
-    new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    });
+      new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      });
 
     return getPosition().then((position) => {
       const lat = position.coords.latitude;
@@ -22,20 +68,20 @@ export default class TRANSIT_API {
       const location = { lat, lng };
       const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${lat},${lng}&key=${googleMapsKey}`;
       return fetch(url)
-      .then(response => response.json())
-      .then((data) => {
-        location.address = data.results[0].formatted_address;
-        return location;
-      })
-      .catch(e => e);
+        .then(response => response.json())
+        .then((data) => {
+          location.address = data.results[0].formatted_address;
+          return location;
+        })
+        .catch(e => e);
     });
   }
 
   static fetchAlerts() {
-    return fetch(
-      `${apiUrl}/alerts`,
-    )
-    .then(response => response.json())
-    .catch(e => e);
+    return fetch(`${apiUrl}/alerts`)
+      .then(response => response.json())
+      .catch((e) => {
+        console.log('ERROR IN FETCH ALERTS', e);
+      });
   }
 }
