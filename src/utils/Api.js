ca311109f591c95c559c22cbefe32c6594e77317
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-14 18:16:25 -0700

    Implement helper functions in api calls

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,3 +1,5 @@
+import HELPERS from './helpers';
+
 const googleMapsKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 const apiUrl = process.env.REACT_APP_API_SERVER_URL;
 
@@ -7,49 +9,7 @@ export default class TRANSIT_API {
       `${apiUrl}/directions?origin=${origin}&destination=${destination}&alternatives=true`,
     )
       .then(response => response.json())
-      .then(json =>
-        json.map((rawJourneyObj) => {
-          const journeyObj = {
-            destination: rawJourneyObj.legs[0].end_address,
-            arrivalTimeText: rawJourneyObj.legs[0].arrival_time.text,
-            departureTimeUTC: rawJourneyObj.legs[0].departure_time.value,
-
-            transitSteps: rawJourneyObj.legs[0].steps.map((step) => {
-              const mode = step.travel_mode;
-              const duration = step.duration.text;
-              const instruction = step.html_instructions;
-
-              const agency = step.transit_details ? step.transit_details.line.agencies[0].name : '';
-              const line = step.transit_details ? step.transit_details.line.short_name : 'N/A';
-              const vehicle = step.transit_details ? step.transit_details.line.vehicle.name : 'N/A';
-              const shortName = step.transit_details ? step.transit_details.line.short_name : ''; // mostly for buses
-              const longName = step.transit_details ? step.transit_details.line.name : ''; // mostly for BART
-              const headsign = step.transit_details ? step.transit_details.headsign : '';
-              const icon = step.transit_details ? step.transit_details.line.icon : 'N/A'; // mostly for buses
-              const localIcon = step.transit_details
-                ? step.transit_details.line.vehicle.local_icon
-                : 'N/A'; // mostly for BART
-
-              const stepObj = {
-                mode,
-                duration,
-                instruction,
-                agency,
-                line,
-                vehicle,
-                shortName,
-                longName,
-                headsign,
-                icon,
-                localIcon,
-              };
-              return stepObj;
-            }),
-          };
-
-          return journeyObj;
-        }),
-      )
+      .then(json => HELPERS.createArrayOfJourneyObjects(json))
       .then(journeys => journeys)
       .catch((e) => {
         console.dir('ERROR IN FETCH JOURNEYS', e);
