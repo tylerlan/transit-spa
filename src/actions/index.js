f0b745ebd893e302de0def3d3396c9ffa3e6412b
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-14 18:16:02 -0700

    Implement helper functions in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -45,17 +45,9 @@ export function removeDestination(destinationId) {
 }
 
 export function refreshJourneys(destinationId, origin, destination) {
-  return async (dispatch, getState, { TRANSIT_API }) => {
+  return async (dispatch, getState, { TRANSIT_API, HELPERS }) => {
     const journeys = await TRANSIT_API.fetchJourneys(origin, destination);
-
-    const journeysOffset = journeys
-      .filter((journey) => {
-        const currentTimeInSeconds = Date.now() / 1000;
-        const diff = journey.departureTimeUTC - currentTimeInSeconds;
-        const offset = 1.5;
-        return diff >= offset;
-      })
-      .sort((a, b) => a - b);
+    const journeysOffset = await HELPERS.offsetJourneys(journeys);
 
     dispatch(removeJourneys(destinationId));
     dispatch(addJourneys(destinationId, journeysOffset));
@@ -63,47 +55,12 @@ export function refreshJourneys(destinationId, origin, destination) {
 }
 
 export function fetchJourneys(destinationId, origin, destination) {
-  return async (dispatch, getState, { TRANSIT_API }) => {
+  return async (dispatch, getState, { TRANSIT_API, HELPERS }) => {
     const journeys = await TRANSIT_API.fetchJourneys(origin, destination);
     const alerts = await TRANSIT_API.fetchAlerts();
 
-    journeys.forEach((journey) => {
-      journey.alerts = [];
-      if (!alerts) {
-        // if there are no no alerts, say it's on-time
-        journey.alerts[0] = 'on-time';
-        return;
-      }
-
-      // loop through the transit steps looking for potential affected lines
-      journey.transitSteps.forEach((transitStep) => {
-        // loop through the alert objects
-        Object.keys(alerts).forEach((index) => {
-          // if there are affected lines for one alert object
-          if (alerts[index].affectedLines.length) {
-            // check to see if the line in the step...
-            const journeyStepLine = transitStep.line;
-            // ...is included in the alert
-            if (alerts[index].affectedLines.includes(journeyStepLine)) {
-              // if it is, push the alert description
-              journeys.alerts.push(index.description);
-            }
-            // if it isn't, do nothing
-          }
-        });
-      });
-      // if this journey has no alerts, say that it is on-time
-      if (!journey.alerts.length) journey.alerts[0] = 'on-time';
-    });
-
-    const journeysOffset = journeys
-      .filter((journey) => {
-        const currentTimeInSeconds = Date.now() / 1000;
-        const diff = journey.departureTimeUTC - currentTimeInSeconds;
-        const offset = 1.5;
-        return diff >= offset;
-      })
-      .sort((a, b) => a - b);
+    const journeysWithAlerts = await HELPERS.applyAlerts(journeys, alerts);
+    const journeysOffset = await HELPERS.offsetJourneys(journeysWithAlerts);
 
     dispatch(removeJourneys(destinationId));
     dispatch(addJourneys(destinationId, journeysOffset));
