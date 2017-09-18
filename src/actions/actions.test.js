49a6072d6db6936e8b36d83f77c86ad5e98fbf3c
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-18 12:58:12 -0700

    Add test for helper functions getting called in fetchJourneys

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -129,14 +129,6 @@ describe('actions', () => {
     const theFuture = Date.now() + 500;
     const mockApiFetchJourneys = jest.fn();
 
-    // const mockCreateArrayOfJourneyObjects = jest.fn();
-    // const mockApplyAlerts = jest.fn();
-    // const mockOffsetJourneys = jest.fn();
-    //
-    // expect(mockCreateArrayOfJourneyObjects).toHaveBeenCalled();
-    // expect(mockApplyAlerts).toHaveBeenCalled();
-    // expect(mockOffsetJourneys).toHaveBeenCalled();
-
     mockApiFetchJourneys.mockReturnValue(
       Promise.resolve([
         {
@@ -407,5 +399,145 @@ describe('actions', () => {
     return store.dispatch(actions.fetchJourneys(5, 'home', 'work')).then(() => {
       expect(store.getActions()).toEqual(expectedActions);
     });
+  });
+
+  it('should removeJourneys and addJourneys and alerts when there are already journeys in the state for a destination', () => {
+    const theFuture = Date.now() + 500;
+
+    const initialState = {
+      widgets: {
+        ids: ['transit'],
+        byId: {
+          transit: {
+            configuration: {
+              currentLocation: {
+                address: '44 Tehama St, San Francisco, CA 94105',
+              },
+            },
+            destinations: {
+              ids: [5],
+              byId: {
+                5: {
+                  id: 5,
+                  address: 'SFO, San Francisco, CA 94128',
+                },
+              },
+            },
+            journeys: {
+              byDestinationId: {},
+            },
+            alerts: {},
+          },
+        },
+      },
+    };
+    const json = [
+      {
+        legs: [
+          {
+            end_address: '123 Main st',
+            arrival_time: { text: '12:00pm' },
+            departure_time: { value: theFuture },
+            steps: [
+              {
+                html_instructions: 'Walk to Montgomery St. Station',
+                travel_mode: 'WALKING',
+                duration: { text: '8 mins' },
+                line: 'N/A',
+                vehicle: 'N/A',
+              },
+            ],
+          },
+        ],
+      },
+    ];
+    const alerts = {
+      1: {
+        affectedLines: ['18', '52'],
+        description:
+          'Due to construction, Lines 18 and 52 will not serve any stops on Monroe Street between Jackson Street and San Pablo Avenue..',
+        subject:
+          'Lines 18 and 52 - Stop Closures near UC Village on Monroe Street and San Pablo Avenue',
+      },
+    };
+    const journeys = [
+      {
+        destination: '123 Main st',
+        arrivalTimeText: '12:00pm',
+        departureTimeUTC: theFuture,
+        transitSteps: [
+          {
+            agency: '',
+            duration: '8 mins',
+            headsign: '',
+            icon: 'N/A',
+            instruction: 'Walk to Montgomery St. Station',
+            line: 'N/A',
+            localIcon: 'N/A',
+            longName: '',
+            mode: 'WALKING',
+            shortName: '',
+            vehicle: 'N/A',
+          },
+        ],
+      },
+    ];
+    const journeysWithAlerts = [
+      {
+        alerts: ['on-time'],
+        destination: '123 Main st',
+        arrivalTimeText: '12:00pm',
+        departureTimeUTC: theFuture,
+        transitSteps: [
+          {
+            agency: '',
+            duration: '8 mins',
+            headsign: '',
+            icon: 'N/A',
+            instruction: 'Walk to Montgomery St. Station',
+            line: 'N/A',
+            localIcon: 'N/A',
+            longName: '',
+            mode: 'WALKING',
+            shortName: '',
+            vehicle: 'N/A',
+          },
+        ],
+      },
+    ];
+
+    const mockApiFetchJourneys = jest.fn();
+    mockApiFetchJourneys.mockReturnValue(Promise.resolve(json));
+
+    const mockApiFetchAlerts = jest.fn();
+    mockApiFetchAlerts.mockReturnValue(Promise.resolve(alerts));
+
+    HELPERS.createArrayOfJourneyObjects = jest.fn(() => Promise.resolve(journeys));
+
+    HELPERS.applyAlerts = jest.fn(() => Promise.resolve(journeysWithAlerts));
+
+    HELPERS.offsetJourneys = jest.fn(() => Promise.resolve(journeysWithAlerts));
+
+    const extraArgument = {
+      TRANSIT_API: {
+        fetchJourneys: mockApiFetchJourneys,
+        fetchAlerts: mockApiFetchAlerts,
+      },
+    };
+
+    const mockStore = configureStore([thunk.withExtraArgument(extraArgument)]);
+    const store = mockStore(initialState);
+
+    return store
+      .dispatch(actions.fetchJourneys(5, 'home', 'work'))
+      .then(() => {
+        expect(HELPERS.createArrayOfJourneyObjects).toBeCalledWith(json);
+      })
+      .then(() => {
+        expect(HELPERS.applyAlerts).toBeCalledWith(journeys, alerts);
+      })
+      .then(() => {
+        expect(HELPERS.offsetJourneys).toBeCalledWith(journeysWithAlerts);
+      });
   });
 });
