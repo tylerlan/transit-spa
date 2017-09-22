b36f2f3e1b3ba00671947932e21f094e6a5ba284
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-09-21 23:19:07 -0700

    Add tests for journey helper functions

---
+++ src/utils/journeyHelpers.test.js
@@ -0,0 +1,51 @@
+import { formatAgency, getInstructions, formatTransitGraphic } from './journeyHelpers';
+
+const bartstep = {
+  agency: 'Bay Area Rapid Transit',
+  duration: '14 mins',
+  headsign: 'Pittsburg/Bay Point',
+  icon: undefined,
+  instruction: 'Metro rail towards Pittsburg/Bay Point',
+  line: undefined,
+  localIcon: '//maps.gstatic.com/mapfiles/transit/iw2/6/us-ca-bart.png',
+  longName: 'Pittsburg/Bay Point - SFIA/Millbrae',
+  mode: 'TRANSIT',
+  shortName: undefined,
+  vehicle: 'Metro rail',
+};
+
+const vtaStep = {
+  agency: 'VTA',
+  duration: '24 mins',
+  headsign: 'Great Mall',
+  icon: undefined,
+  instruction: 'Bus towards great Mall',
+  line: undefined,
+  localIcon: undefined,
+  longName: undefined,
+  mode: 'TRANSIT',
+  shortName: '70',
+  vehicle: 'Bus',
+};
+
+describe('formatAgency', () => {
+  it('should replace Bay Area Rapid Transit with BART', () => {
+    expect(formatAgency(bartstep)).toEqual('BART');
+  });
+});
+
+describe('getInstructions', () => {
+  it('should exist', () => {
+    expect(getInstructions(bartstep)).toEqual(bartstep.instruction);
+  });
+});
+
+describe('formatTransitGraphic', () => {
+  it('returns object with correct keys', () => {
+    expect(formatTransitGraphic('VTA', {})).toEqual({
+      iconURL: null,
+      lineColor: null,
+      transitLine: null,
+    });
+  });
+});
